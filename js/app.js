e3d607a367dc42bd4ae1daeadc25a6ec606fca6f
Josh Briand <joshbriand@gmail.com>
2017-11-20 14:12:59 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,122 @@
+var downContent = '';
+var downLink = '';
+var downSection = '';
+
+$('#joshbriand').click(function(){
+	if (downContent !== '') {
+		up(downContent, downLink);
+	}
+})
+
+$('#navSpacer').click(function(){
+	if (downContent !== '') {
+		up(downContent, downLink);
+	}
+})
+
+$('#webDevLink').click(function(){
+	if (downSection === 'webDev') {
+		up(downContent, downLink);
+		$('#actualSlogan').fadeIn(1200);
+	} else if (downSection !== '') {
+		downContent.slideUp('slow', function(){
+			up(downContent, downLink);
+			$('#actualSlogan').fadeOut(400);
+			$('#webDevContent').slideDown('slow');
+			$('#webDevContent').css('display','flex');
+			$('#webDevLink').css('text-decoration', 'underline');
+			$('#merchLink').css('color', 'rgba(0,0,0,.15)');
+			$('#contactLink').css('color', 'rgba(0,0,0,.15)');
+			downContent = $('#webDevContent');
+			downLink = $('#webDevLink');
+			downSection = 'webDev';
+		});
+	} else {
+		$('#actualSlogan').fadeOut(400);
+		$('#webDevContent').slideDown('slow');
+		$('#webDevContent').css('display','flex');
+		$('#webDevLink').css('text-decoration', 'underline');
+		$('#merchLink').css('color', 'rgba(0,0,0,.15)');
+		$('#contactLink').css('color', 'rgba(0,0,0,.15)');
+		downContent = $('#webDevContent');
+		downLink = $('#webDevLink');
+		downSection = 'webDev';
+	};
+});
+
+
+$('#merchLink').click(function(){
+	if (downSection === 'merch') {
+		up(downContent, downLink);
+		$('#actualSlogan').fadeIn(1200);
+	} else if (downSection !== '') {
+		downContent.slideUp('slow', function(){
+			up(downContent, downLink);
+			$('#actualSlogan').fadeOut(400);
+			$('#merchContent').slideDown('slow');
+			$('#merchContent').css('display','flex');
+			$('#merchLink').css('text-decoration', 'underline');
+			$('#webDevLink').css('color', 'rgba(0,0,0,.15)');
+			$('#contactLink').css('color', 'rgba(0,0,0,.15)');
+			downContent = $('#merchContent');
+			downLink = $('#merchLink');
+			downSection = 'merch';
+		});
+	} else {
+		$('#actualSlogan').fadeOut(400);
+		$('#merchContent').slideDown('slow');
+		$('#merchContent').css('display','flex');
+		$('#merchLink').css('text-decoration', 'underline');
+		$('#webDevLink').css('color', 'rgba(0,0,0,.15)');
+		$('#contactLink').css('color', 'rgba(0,0,0,.15)');
+		downContent = $('#merchContent');
+		downLink = $('#merchLink');
+		downSection = 'merch';
+	};
+});
+
+
+$('#contactLink').click(function(){
+	if (downSection === 'contact') {
+		up(downContent, downLink);
+		$('#actualSlogan').fadeIn(1200);
+	} else if (downSection !== '') {
+		downContent.slideUp('slow', function(){
+			up(downContent, downLink);
+			$('#actualSlogan').fadeOut(400);
+			$('#contactContent').slideDown('slow');
+			$('#contactContent').css('display','flex');
+			$('#contactLink').css('text-decoration', 'underline');
+			$('#webDevLink').css('color', 'rgba(0,0,0,.15)');
+			$('#merchLink').css('color', 'rgba(0,0,0,.15)');
+			downContent = $('#contactContent');
+			downLink = $('#contactLink');
+			downSection = 'contact';
+		});
+	} else {
+		$('#actualSlogan').fadeOut(400);
+		$('#contactContent').slideDown('slow');
+		$('#contactContent').css('display','flex');
+		$('#contactLink').css('text-decoration', 'underline');
+		$('#webDevLink').css('color', 'rgba(0,0,0,.15)');
+		$('#merchLink').css('color', 'rgba(0,0,0,.15)');
+		downContent = $('#contactContent');
+		downLink = $('#contactLink');
+		downSection = 'contact';
+	};
+});
+
+function up (content, link) {
+	if (content !== '') {
+		content.slideUp('slow');
+	};
+	$('#merchLink').css('color', 'rgba(0,0,0,1)');
+	$('#webDevLink').css('color', 'rgba(0,0,0,1)');
+	$('#contactLink').css('color', 'rgba(0,0,0,1)');
+	$('#merchLink').css('text-decoration', 'none');
+	$('#webDevLink').css('text-decoration', 'none');
+	$('#contactLink').css('text-decoration', 'none');
+	downContent = '';
+	downLink = '';
+	downSection = '';
+};
